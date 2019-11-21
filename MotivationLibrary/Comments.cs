be5a588d1e8f8fb3be67f11738a10f03862e57cb
Myran <myrankalander@gmail.com>
2019-11-21 10:50:04 +0100

    Added some comments and tested it, working!

--- MotivationLibrary/Comments.cs
+++ MotivationLibrary/Comments.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace MotivationLibrary
 {
-    class Comments
+    public class Comments
     {
         public string PositiveComment()
         {
@@ -11,7 +11,7 @@ namespace MotivationLibrary
         positive[1] = "Du är stark och unik! ";
         positive[2] = "Du är fantastisk! ";
         positive[3] = "Bra jobbat! ";
-        positive[4] = "asdqweq wqeqe";
+        positive[4] = "Du är så j*vla bra, jag älskar dig! ";
 
         Random random = new Random();
 
@@ -19,18 +19,22 @@ namespace MotivationLibrary
         
         return positive[index];
         }
-        public void NegativeComment()
+        public string NegativeComment()
         {
-        string[] negative = new string[5];
-        negative[0] = "asd";
-        negative[1] = "asd";
-        negative[2] = "asd";
-        negative[3] = "asd";
-        negative[4] = "asd";
+        string[] negative = new string[8];
+        negative[0] = "Jag vet att du kan bättre... ";
+        negative[1] = "Jag vet koma patienter som gör mer än vad du gör... ";
+        negative[2] = "Jag blir så besviken...";
+        negative[3] = "Din motivation får mig att vilja dö...";
+        negative[4] = "Du är inte bara dålig, du är ful också...";
+        negative[5] = "Seriöst?...";
+        negative[6] = "Okej, en pizza på det?...";
+        negative[7] = "Kvist hade gjort detta bättre...";
 
         Random random = new Random();
 
         int index = random.Next(negative.Length); //GENERERAR ETT SLUPPMÄSSIGT INDEX MINDRE ÄN STORLEKEN PÅ ARRAYEN
+        return negative[index];
         }
     }
 }
