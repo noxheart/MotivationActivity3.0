e0031bac57059de8433a664b4647b47a2dcdb69e
Adam <adam.axelsson1992@gmail.com>
2019-11-25 12:55:51 +0100

    not finished with database

--- MotivationLibrary/Workout.cs
+++ MotivationLibrary/Workout.cs
@@ -6,13 +6,19 @@ namespace MotivationLibrary
     {
         public DateTime WhenWorkOutOccured { get; protected set; }
         public int MinutesWorkedOut { get; protected set; }
-        public double pointsForWorkout { get; protected set; }
-    
+        public double PointsForWorkout { get; protected set; }
+
         public Workout(DateTime whenWorkedOut, int minutesWorkedOut, double points)
         {
             this.WhenWorkOutOccured = whenWorkedOut;
             this.MinutesWorkedOut = minutesWorkedOut;
-            this.pointsForWorkout = points;
+            this.PointsForWorkout = points;
+        }
+        public void AddWorkout()
+        {
+            var db = new Database("Server=40.85.84.155;Database=SKK5;User=Student5;Password=YH-student@2019;");
+            db.AddWorkouts(this);
+
         }
     }
 }
