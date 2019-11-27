31810643a34a1cd1e614c9df4e774f746b044123
Noxheart <daniel-97@live.se>
2019-11-27 14:12:06 +0100

    fixed checkweeks

--- MotivationLibrary/Statistics.cs
+++ MotivationLibrary/Statistics.cs
@@ -26,15 +26,28 @@ namespace MotivationLibrary
                 print.PrintStatistic(workout);
             }
         }
-        public double WorkOutPointsFor(int days, User user)
+        public double WorkOutPointsFor(User user)
         {
             var db = new Database("Server=40.85.84.155;Database=Student5;User=Student5;Password=YH-student@2019;");
             double i = 0;
             foreach (var workout in db.GetWorkouts(user))
             {
+                bool sameWeek = CheckWeeks(workout.WhenWorkOutOccured);
+                if (sameWeek == true)
+                {
+                    i = i + workout.PointsForWorkout;
+                }
             }
             //TODO skicka rätt double
             return i;
+        }
+        public bool CheckWeeks(DateTime workOutDate)
+        {
+            DateTimeFormatInfo dateTimeFormat = CultureInfo.GetCultureInfo("sv-SE").DateTimeFormat;
+            Calendar calendar = dateTimeFormat.Calendar;
+            int workOutWeek = calendar.GetWeekOfYear(workOutDate, dateTimeFormat.CalendarWeekRule, dateTimeFormat.FirstDayOfWeek);
+            int currentWeek = calendar.GetWeekOfYear(DateTime.Now, dateTimeFormat.CalendarWeekRule, dateTimeFormat.FirstDayOfWeek);
+            return workOutWeek == currentWeek;
         }
     }
 }
